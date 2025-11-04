6ebd4b76f36f6e1dff1a3fc76604169376ad79e5
deeptanshukumar <deeptanshu.kumar13@gmail.com>
2025-11-04 17:47:24 +0530

    Update Google verification code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <meta name="google-site-verification" content="emMyKBqsSzkydyD5KIyFQ3iVDAp6nfHs-aY_g3e1j0I" />
+        <meta name="google-site-verification" content="c1NrV2S56uUJCt1IxsATy8eXspQXRwLGzIA6BniCIrs" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/icon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
